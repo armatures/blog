3faafa6cb811d0a7505cf0c9e1d20b19a1132643
Charlie Bevis <charliebevis@gmail.com>
2019-12-18 11:15:50 -0600

    copy mscgen-images to the server

--- site.hs
+++ site.hs
@@ -25,6 +25,10 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
+    match "mscgen-images/*.svg" $ do
+        route   idRoute
+        compile copyFileCompiler
+
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
