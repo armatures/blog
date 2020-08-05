70e32d6960d74e3efd15a8f684565682b13ea971
Charlie Bevis <charliebevis@gmail.com>
2020-08-05 16:52:01 -0500

    allow nested directories in static/

--- site.hs
+++ site.hs
@@ -29,7 +29,7 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "static/*" $ do
+    match "static/**" $ do
         route   idRoute
         compile copyFileCompiler
 
